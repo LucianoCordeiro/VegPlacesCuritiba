10929bde4be9ab9a796a3cb177526366a6082161
Luciano Cesar <lucianocordeiro1991@hotmail.com>
2017-12-11 11:20:35 -0200

    Butterfree

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,18 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: 'lucianocesarcordeiro@gmail.com',
+  password: 'tart1991', #For security purposes, I removed my password
+  authentication: "plain",
+  enable_starttls_auto: true
+}
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
