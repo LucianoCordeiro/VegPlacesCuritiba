8379dde7088fa2224b042001a7d172c9ba8e79dd
Luciano Cesar <lucianocordeiro1991@hotmail.com>
2017-12-11 11:51:53 -0200

    Weedle

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,10 +31,20 @@ Rails.application.configure do
   config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
+  config.action_mailer.default_url_options = { :host => 'pacific-plateau-24430.herokuapp.com' }
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: 'lucianocesarcordeiro@gmail.com',
+  password: 'tart1991', #For security purposes, I removed my password
+  authentication: "plain",
+  enable_starttls_auto: true
+  }
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
