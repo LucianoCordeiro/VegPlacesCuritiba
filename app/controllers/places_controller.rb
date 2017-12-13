03d63f542cacf309e9c6b2017975b151fc16e31f
Luciano Cesar <lucianocordeiro1991@hotmail.com>
2017-12-13 12:44:36 -0200

    Butterfree

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,7 @@
 class PlacesController < ApplicationController
 
+  before_action :set_place, only: [:edit, :update, :destroy]
+
   def create
     @place = Place.create(place_params)
     if @place.save
@@ -11,21 +13,36 @@ class PlacesController < ApplicationController
     @places = Place.search(params[:search])
   end
 
-  def show
-    @place = Place.find(params[:id])
+  def edit
+  end
+
+  def update
+    if @place.update(place_params)
+      redirect_to list_url, notice: 'Estabelecimento atualizado'
+    end
   end
 
   def list
     @place = Place.new
     @places = Place.all
   end
-end
+
+  def destroy
+    @place.destroy
+    redirect_to list_url, notice: 'Estabelecimento deletado'
+  end
+
+private
 
 def profs
 end
 
-private
-
 def place_params
   params.require(:place).permit(:name, :modality, :description, :openinghours, :address, :sort, :facebookpage, :search)
+end
+
+def set_place
+  @place = Place.find(params[:id])
+end
+
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,4 @@
 class Place < ApplicationRecord
-  has_and_belongs_to_many :services
 
   def self.search(search)
     if search
